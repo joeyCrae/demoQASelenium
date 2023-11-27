4fe6911ab996644cee4de60df41b093aaf26f2fa
Joel Bamfo <120023804+joeybamfs@users.noreply.github.com>
2023-11-27 14:53:08 +0000

    Added the assertion for the visibility of the elements tab and also clicking the elements tab

--- src/main/java/com/demoqa/Main.java
+++ src/main/java/com/demoqa/Main.java
@@ -1,19 +1,34 @@
 package com.demoqa;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    @Test
+    public void main() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/");
+        driver.manage().window().maximize();
+        System.out.println("Demoqa website opened successfully");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        //Getting the Elements tab and clicking it
+        WebElement elements = driver.findElement(By.xpath("//*[@id='app']/div/div/div[2]/div/div[1]"));
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Assert that the Elements tab is visible
+        Assert.assertTrue(elements.isDisplayed(), "This is not visible");
+
+        //Clicking the elements tab
+        elements.click();
+
+//        driver.quit();
     }
+
+
+
 }
